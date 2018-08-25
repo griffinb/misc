9ceb0b95a1a608f1733c416b169bca57b0efd83b
Brandon Griffin <brandongriffin81@gmail.com>
2018-08-25 09:43:46 -0500

    improvements

--- vagrant_install.sh
+++ vagrant_install.sh
@@ -1,46 +1,52 @@
-apt update
-apt -y dist-upgrade
-
-# Editors
-apt install -y vim
+sudo apt update
+sudo apt -y dist-upgrade
 
 # Git
-apt install -y git
-
-# Curl
-apt install -y curl
-
-# Python
-apt install -y build-essential python-dev libffi-dev libssl-dev zlib1g-dev \
-    libbz2-dev libreadline-dev libsqlite3-dev llvm libncurses5-dev libncursesw5-dev \
-python-pip python3-pip libpq-dev
-pip install virtualenv
-
-# aws command line tools
-pip install awscli
-pip install awslogs
-
-# Docker
-apt update
-apt -y install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg |  apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt update
-apt -y install docker-ce
-apt -y install docker-ce=18.03.0~ce-0~ubuntu
-# Only use if needed:
-# usermod -aG docker $USER
+sudo apt install -y git
 
 # Oh my zsh
-apt install -y zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo apt install -y zsh
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo chown vagrant:vagrant .zshrc
+
+# Editors
+sudo apt install -y vim
+
+# Python
+sudo apt install -y build-essential python-dev libffi-dev libssl-dev zlib1g-dev \
+    libbz2-dev libreadline-dev libsqlite3-dev llvm libncurses5-dev libncursesw5-dev \
+    python-pip python3-pip libpq-dev
 
 # pyenv
 curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
 echo 'export PATH="/home/vagrant/.pyenv/bin:$PATH"' >> ~/.zshrc
 echo 'eval "$(pyenv init -)"' >> ~/.zshrc
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshrc
-source .zshrc
+
+# Change default shell
+sudo passwd vagrant
+chsh -s /bin/zsh # Logout/login after this
+
 pyenv update
 pyenv install 2.7.14
 pyenv install 3.6.3
+# pip install virtualenv
+
+# aws command line tools
+sudo pip install awscli
+sudo pip install awslogs
+
+# Docker
+sudo apt update
+sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg |  sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt -y install docker-ce
+sudo apt -y install docker-ce=18.03.0~ce-0~ubuntu
+# Only use if needed:
+# usermod -aG docker $USER
+
+
+
+
